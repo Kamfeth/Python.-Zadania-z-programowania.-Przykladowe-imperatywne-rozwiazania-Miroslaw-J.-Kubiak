86fa08febc5794d0fb9de6735e6dd94667f4f6cb
Kamil Paradowski <kamil.paradowski@yahoo.com>
2023-02-24 12:21:23 +0100

    Rozdział 1: Zadanie 1

---
+++ Rozdział 1/Zadanie 1/Zadanie.py
@@ -0,0 +1,7 @@
+def main():
+  a = float(input("Wprowadź pierwszy bok prostokąta: "))
+  b = float(input("Wprowadź drugi bok prostokąta: "))
+  pole = a * b
+  print("Pole prostokąta jest równe", pole)
+
+main()
