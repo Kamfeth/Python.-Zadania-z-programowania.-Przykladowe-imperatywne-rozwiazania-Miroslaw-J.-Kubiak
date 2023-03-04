d3622206fb138fc4a95dd25d1253a8fb62d28be8
Kamil Paradowski <kamil.paradowski@yahoo.com>
2023-03-04 23:40:47 +0100

    Rozdział 1: Zadanie 6

---
+++ Rozdział 1/Zadanie 6/Zadanie.py
@@ -0,0 +1,12 @@
+def main():
+  imie = input("Podaj swoje imię: ")
+  nazwisko = input("Podaj swoje nazwisko: ")
+  wiek = int(input(("Podaj swój wiek: ")))
+  cenaChleba = float(input(("Podaj cenę chleba: ")))
+  print("\nOto wprowadzone dane: ")
+  print("Imię:", imie)
+  print("Nazwisko:", nazwisko)
+  print("Wiek:", wiek)
+  print("Cena chleba:", cenaChleba)
+
+main()
