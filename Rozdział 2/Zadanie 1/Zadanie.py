a0724cc9db7a428096c7439b4c55bf0a238e0695
Kamil <53303297+Kamfeth@users.noreply.github.com>
2023-03-12 08:34:28 +0100

    Update Zadanie.py

--- Rozdział 2/Zadanie 1/Zadanie.py
+++ Rozdział 2/Zadanie 1/Zadanie.py
@@ -3,6 +3,6 @@ def main():
   b = int(input("Wprowadź wartość drugiego boku trójkąta: "))
   c = int(input("Wprowadź wartość trzeciego boku trójkąta: "))
   print("Wprowadzone wartości boków ", end = "")
-  print("spełniają równanie Pitagorasa") if a ** 2 + b ** 2 == c ** 2 else print("nie spełniają równania Pitagorasa")
+  print("spełniają równanie Pitagorasa" if a ** 2 + b ** 2 == c ** 2 else "nie spełniają równania Pitagorasa")
 
 main()
